d35d045c3b4a2b3b8190075f8fc13f6b30af2dba
Alexander Nozik <altavir@gmail.com>
2018-08-10 17:07:20 +0300

    Changed to buildSrc. Added tasks for specific chapters

---
+++ buildSrc/src/main/groovy/elabnik/BuildHTMLTask.groovy
@@ -0,0 +1,77 @@
+package elabnik
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.options.Option
+
+class BuildHTMLTask extends DefaultTask {
+    @Option(description = "Source file name or template without `.tex` suffix")
+    String source
+
+    @Option(description = "the name of the target xml/html")
+    String target
+
+    Map parameters = [:]
+
+    File logDir = new File(project.buildDir, "logs")
+
+    @OutputDirectory
+    File xmlDir = new File(project.buildDir, "xml")
+
+    @OutputDirectory
+    File htmlDir = new File(project.buildDir, "html")
+
+    @TaskAction
+    def run() {
+        if(source == null){
+            throw new RuntimeException("Source file is undefined")
+        }
+        def target = target ?: source
+
+        File texFile = project.file(source + ".tex")
+        def sourceFile = Utils.loadTex(project, texFile, target, parameters)
+
+        def xmlFile = new File(xmlDir, "${target}.xml")
+
+        logDir.mkdirs()
+        xmlDir.mkdirs()
+        //Customize command for linux
+        def command = ["wsl"]
+        def xmlLine = command + ["latexml",
+                                 "--destination=\"${Utils.linuxify(project, xmlFile.absolutePath)}\"",
+                                 "--inputencoding=utf8",
+                                 "\"${Utils.linuxify(project, sourceFile.absolutePath)}\""
+        ]
+
+        logger.info("Preparing xml")
+        logger.info("Using command line for latexml $target: ${xmlLine.join(" ")}")
+        project.exec {
+            workingDir '.'
+            errorOutput(new FileOutputStream(new File(logDir, "${target}.xml.log")))
+            //TODO убрать первый аргумент для линукса
+            commandLine xmlLine
+        }
+
+        htmlDir.mkdirs()
+
+        def htmlLine = command + [
+                "latexmlpost",
+                "--destination=${Utils.linuxify(project, htmlDir.toString())}/${xmlFile.name.replaceFirst(~/\.[^.]+$/, '')}.html",
+                "--format=html5",
+                "--split",
+                "--splitat=section",
+                "--javascript='http://fred-wang.github.io/mathjax.js/mpadded-min.js'",
+                Utils.linuxify(project, xmlFile.absolutePath)
+        ]
+
+        logger.info("Command line for latexmlpost $target: ${htmlLine.join(" ")}")
+
+        project.exec {
+            workingDir '.'
+            errorOutput(new FileOutputStream(new File(logDir, "${target}.html.log")))
+            commandLine htmlLine
+
+        }
+    }
+}
---
+++ buildSrc/src/main/groovy/elabnik/SvgTask.groovy
@@ -0,0 +1,6 @@
+package elabnik
+
+import org.gradle.api.DefaultTask
+
+class SvgTask extends DefaultTask {
+}
